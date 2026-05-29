0faea5d742edc5e7b61e8597a32025b2600849cc
ShiroOnigami23 <shiroonigami23@gmail.com>
2026-05-29 10:59:32 +0530

    Fix resources auth bootstrap and replace Tailwind CDN with built CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        './index.html',
+        './html/**/*.html',
+        './js/**/*.js',
+        './admin/**/*.js',
+        './admin/**/*.html'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Manrope', 'sans-serif']
+            }
+        }
+    },
+    plugins: []
+};
